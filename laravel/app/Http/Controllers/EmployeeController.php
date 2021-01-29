869b8c4fcd70adb915d429205fed8273caa592c8
Anna <rosalenanna@gmail.com>
2021-01-29 15:18:36 +0100

    problema access denied

---
+++ laravel/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+use App\Employee;
+
+class EmployeeController extends Controller
+{
+    public function index(){
+
+        $employees = Employee::all();
+        return view('pages.emps', compact('employees'));
+
+    }
+}
